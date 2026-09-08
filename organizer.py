233c03665f9253a889cc892329b60d70a6b5da6e
rinana7 <sakairinana@gmail.com>
2026-09-07 16:46:59 -1000

    define category extension mappings dictionary

--- organizer.py
+++ organizer.py
@@ -1,3 +1,12 @@
 import os
 import shutil
 from pathlib import Path
+
+CATEGORIES = {
+    "Images":[".jpg", ".jpeg", ".png", ".gif", ".svg"],
+    "Documents": [".pdf", ".docx", ".txt", ".xlsx", ".csv"],
+    "Audio": [".mp3", ".wav", ".flac"],
+    "Videos": [".mp4", ".mkv", ".mov"],
+    "Archives": [".zip", ".tar", ".gz", ".7z"],
+    "Executables": [".exe", ".dmg", ".pkg", ".sh"]
+}
